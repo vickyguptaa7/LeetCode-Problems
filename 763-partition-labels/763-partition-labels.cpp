2d16153118add35bf1a4da7689c952f81050514e
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-01-31 09:43:10 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (8.36%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    vector<pair<int,int>>Interval(string s)
+    {
+        unordered_map<int,pair<int,int>>umap;
+        int n=s.length();
+        for(int i=0;i<n;i++)
+        {
+            if(umap.find(s[i])==umap.end())
+            {
+                umap[s[i]]={i,i+1};
+            }
+            else
+            {
+                umap[s[i]].second=i+1;
+            }
+        }
+        vector<pair<int,int>>interval;
+        for(auto &x:umap)
+        {
+            interval.push_back(x.second);
+        }
+        sort(interval.begin(),interval.end());
+        return interval;
+    }
+    vector<pair<int,int>>Merge_Interval(vector<pair<int,int>>&Interval)
+    {
+        vector<pair<int,int>>newInterval;
+        stack<pair<int,int>>stack;
+        stack.push(Interval[0]);
+        int n=Interval.size();
+        for(int i=1;i<n;i++)
+        {
+            pair<int,int>itrvl=stack.top();
+            stack.pop();
+            if(Interval[i].first<itrvl.second)
+            {
+                itrvl.second=max(itrvl.second,Interval[i].second);
+                stack.push(itrvl);
+            }
+            else
+            {
+                stack.push(itrvl);
+                stack.push(Interval[i]);
+            }
+        }
+        cout<<stack.size()<<"\n";
+        while(!stack.empty())
+        {
+            newInterval.push_back(stack.top());
+            stack.pop();
+        }
+        sort(newInterval.begin(),newInterval.end());
+        return newInterval;
+    }
+    vector<int> partitionLabels(string s) {
+        int n=s.length();
+        
+        vector<pair<int,int>>interval=Interval(s);
+        
+        vector<pair<int,int>>merge_interval=Merge_Interval(interval);
+        vector<int>result;
+        for(auto x:merge_interval)
+        {
+            result.push_back(x.second-x.first);
+        }
+        return result;
+    }
+};
