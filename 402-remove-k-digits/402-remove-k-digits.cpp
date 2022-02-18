d8e052c899d6a7e1817c4700efa534f0233794ad
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-02-18 11:49:05 +0530

    Time: 48 ms (15.01%), Space: 291.9 MB (8.29%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -1,34 +1,65 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        for(int i=1;i<num.length();i++)
+        stack<char>st;
+        int n=num.length();
+        for(int i=0;i<n;i++)
         {
-            if(num[i-1]>num[i])
+            while(!st.empty()&&k>0&&st.top()>num[i])
             {
-                num.erase(i-1,1);
+                st.pop();
                 k--;
-                if(i>=2)
-                i-=2;
-                else
-                    i--;
             }
-            if(k==0)
+            st.push(num[i]);
+        }
+        string res="";
+        while(!st.empty())
+        {
+            if(k>0)
             {
-                break;
+                k--;
+                st.pop();
+                continue;
             }
+            res=st.top()+res;
+            st.pop();
         }
-        
-        num=num.substr(0,num.length()-k);
-        
-        int iter=0;
-        while(num[iter]=='0')
+        while(res[0]=='0')
         {
-            num.erase(0,1);
+            res.erase(0,1);
         }
-        if(num.length()==0)
-        {
-            return "0";
-        }
-        return num;
+        if(res.size()==0)return "0";
+        return res;
     }
+//         string removeKdigits(string num, int k) {
+//         for(int i=1;i<num.length();i++)
+//         {
+//             if(num[i-1]>num[i])
+//             {
+//                 num.erase(i-1,1);
+//                 k--;
+//                 if(i>=2)
+//                 i-=2;
+//                 else
+//                     i--;
+//             }
+//             if(k==0)
+//             {
+//                 break;
+//             }
+//         }
+        
+//         num=num.substr(0,num.length()-k);
+        
+//         int iter=0;
+//         while(num[iter]=='0')
+//         {
+//             num.erase(0,1);
+//         }
+//         if(num.length()==0)
+//         {
+//             return "0";
+//         }
+//         return num;
+//     }
 };
