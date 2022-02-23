5b6f2efdb7a6ce7369568e1f12e2fe2fdbc450bb
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-02-23 10:30:13 +0530

    Time: 73 ms (66.87%), Space: 33.6 MB (78.37%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -2,18 +2,43 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         // First Way Is To Sort And Find T.C -> O(nlogn)
+        
         // Another Way Is To Use A Map DS For Storing The Freq
+        // vector<int>res;
+        // unordered_set<int>uset;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(uset.find(nums[i])!=uset.end())
+        //     {
+        //         res.push_back(nums[i]);
+        //     }
+        //     else
+        //     {
+        //         uset.insert(nums[i]);
+        //     }
+        // }
+        // return res;
+        
+        // Another Space Optemize Soln Mark Repeated As -ve sign
         vector<int>res;
-        unordered_set<int>uset;
-        for(int i=0;i<nums.size();i++)
+        int n=nums.size();
+        for(int i=0;i<n;i++)
         {
-            if(uset.find(nums[i])!=uset.end())
+            if(nums[i]<0)
             {
-                res.push_back(nums[i]);
+                if(nums[-nums[i]-1]<0)
+                {
+                    res.push_back(-nums[i]);
+                }
+                else nums[-nums[i]-1]=-nums[-nums[i]-1];
             }
             else
             {
-                uset.insert(nums[i]);
+                if(nums[nums[i]-1]<0)
+                {
+                    res.push_back(nums[i]);
+                }
+                else nums[nums[i]-1]=-nums[nums[i]-1];
             }
         }
         return res;
