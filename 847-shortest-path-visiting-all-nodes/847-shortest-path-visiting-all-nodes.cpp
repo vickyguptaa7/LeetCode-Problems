0e3090c35aa6dc0214d51579fe94e3584562200d
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-02-26 18:36:39 +0530

    Time: 173 ms (30.04%), Space: 18.1 MB (45.22%) - LeetHub

---
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+    
+        
+        int graphSize=graph.size();
+        if(graphSize==1)return 0;
+        
+        // Contains node mask cost
+        queue<pair<int,pair<int,int>>>que;
+        
+        set<pair<int,int>>visited;
+        
+        int allVisited=(1<<graphSize)-1;
+        
+        for(int i=0;i<graphSize;i++)
+        {
+            int mask=(1<<i);
+            que.push({i,{mask,0}});
+            visited.insert({i,mask});
+        }
+        
+        while(!que.empty())
+        {
+            int mask=que.front().second.first;
+            int cost=que.front().second.second;
+            int parent=que.front().first;
+            que.pop();
+            for(auto child:graph[parent])
+            {
+                int Mask=(1<<child)|mask;
+                int Cost=cost+1;
+            if(Mask==allVisited)
+            {
+                return Cost;
+            }
+                if(visited.count({child,Mask})==0)
+                {
+                    que.push({child,{Mask,Cost}});
+                    visited.insert({child,Mask});
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
