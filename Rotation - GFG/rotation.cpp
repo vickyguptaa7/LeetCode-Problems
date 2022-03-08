1af8880a5e218ab21764fe29548524bf2b4e1329
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-03-08 10:20:43 +0530

    Added solution - LeetHub

---
+++ Rotation - GFG/rotation.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int findKRotation(int arr[], int n) {
+	    // code here
+	    int low=0,high=n-1;
+	    while(low<high)
+	    {
+	        int mid=low+(high-low)/2;
+	        if(arr[mid]<arr[high])
+	        {
+	            high=mid;
+	        }
+	        else
+	        {
+	            low=mid+1;
+	        }
+	    }
+	    return low;
+	}
+
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findKRotation(a, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
