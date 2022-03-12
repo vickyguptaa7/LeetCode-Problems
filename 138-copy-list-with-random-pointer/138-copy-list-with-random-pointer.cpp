9354492442ef6587b96ce578101dc24af00749bc
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-03-12 20:05:17 +0530

    Time: 14 ms (45.54%), Space: 11.2 MB (87.91%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,6 +17,43 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        if(head==nullptr)return nullptr;
+        Node*curr=head;
+        while(curr)
+        {
+            Node*newnode=new Node(curr->val);
+            newnode->next=curr->next;
+            curr->next=newnode;
+            curr=curr->next->next;
+        }
+        curr=head;
+        while(curr)
+        {
+            if(curr->random)
+                curr->next->random=curr->random->next;
+            else
+                curr->next->random=nullptr;
+            
+            curr=curr->next->next;
+        }
+        Node*newHead=head->next;
+        curr=newHead;
+        while(curr&&curr->next)
+        {
+            head->next=curr->next;
+            curr->next=curr->next->next;
+            curr=curr->next;
+            head=head->next;
+        }
+        head->next=nullptr;
+        return newHead;
+    }
+};
+
+/*
+class Solution { // O(n) Space Used In Map
+public:
+    Node* copyRandomList(Node* head) {
         unordered_map<Node*,Node*>umap;
         Node*curr=head;
         while(curr)
@@ -35,3 +72,4 @@ public:
         return umap[head];
     }
 };
+*/
