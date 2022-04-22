e73ad22aac67b18e4d075dc5e0015cd9e8190305
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-04-22 10:40:06 +0530

    Time: 251 ms (33.90%), Space: 342.3 MB (5.34%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        map<int,int>freqSort;
+        deque<int>maxDeq,minDeq;
         int low=0,high=0,size=nums.size(),res=0;
         while(high<size)
         {
-            freqSort[nums[high++]]++;
-            
-            if(freqSort.rbegin()->first-freqSort.begin()->first>limit)
+                           
+            while(!minDeq.empty()&&nums[minDeq.front()]>=nums[high])
             {
-                res=max(res,high-low-1);
-                freqSort[nums[low]]--;
-                if(freqSort[nums[low]]==0)
-                {
-                    freqSort.erase(nums[low]);
-                }
-                low++;
+                minDeq.pop_front();
             }
+
+                minDeq.push_front(high);
+            
+            while(!maxDeq.empty()&&nums[maxDeq.front()]<=nums[high])
+            {
+                maxDeq.pop_front();
+            }
+                
+
+                maxDeq.push_front(high);
+               
+            high++;
+            if(nums[maxDeq.back()]-nums[minDeq.back()]>limit)
+            {
+               
+                if(!minDeq.empty()&&minDeq.back()==low)
+                {
+                    minDeq.pop_back();
+                }
+                if(!maxDeq.empty()&&maxDeq.back()==low)
+                {
+                    maxDeq.pop_back();
+                } 
+                low++;
+                 res=max(res,high-low);
+            }
+            
         }
         res=max(res,high-low);
         return res;
