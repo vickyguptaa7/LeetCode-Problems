943711ec4c59ca067b8d445a2974fa6f108285a9
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-05-21 09:37:44 +0530

    Time: 225 ms (13.56%), Space: 49 MB (15.29%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -26,16 +26,29 @@ class Solution
     int coinChange(vector<int> &coins, int amount)
     {
         int n = coins.size();
-        vector<vector < int>> dp(n + 1, vector<int> (amount + 1, -1));
-        int ans=dfs_helper(amount, 0, coins, dp);
-        return ans==1e9?-1:ans;
-       	//         for(int i=0;i<n+1;i++)
-       	//         {
-       	//             dp[i][0]=0  ;
-       	//         }
-       	//         for(int i=0;i<amount+1;i++)
-       	//         {
+        vector<vector < int>> dp(n + 1, vector<int> (amount + 1, 0));
+        // int ans = dfs_helper(amount, 0, coins, dp);
+        // return ans == 1e9 ? -1 : ans;
 
-       	//         }
+        for (int i = 1; i < amount + 1; i++) 
+        {
+            dp[0][i]=1e9;
+        }
+        
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<amount+1;j++)
+            {
+                if(j>=coins[i-1])
+                {
+                    dp[i][j]=min(dp[i][j-coins[i-1]]+1,dp[i-1][j]);
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amount]==1e9?-1:dp[n][amount];
     }
 };
