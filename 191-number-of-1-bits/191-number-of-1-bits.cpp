2357bfdc5ada24c5db4889473be46417cf05c50b
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-05-26 08:59:13 +0530

    Time: 4 ms (36.04%), Space: 6 MB (48.74%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int result=0;
+        while(n>0)
+        {
+            result+=(n&1);
+            n=(n>>1);
+        }
+        return result;
+    }
+};
