b0951dff924ee7b2d58f0c6a1678ade27a5cb8be
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-06-13 09:46:15 +0530

    Time: 13 ms (22.10%), Space: 8.5 MB (91.19%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,16 +1,33 @@
 class Solution
 {
-    int dp[201][201];
+    // Using Memoization
+    // int dp[201][201];
     public:
-    Solution(){
-        memset(dp, -1, sizeof dp); 
-    }
-    int minimumTotal(vector<vector < int>> &triangle, int c = 0, int r = 0)
+//     Solution(){
+//         memset(dp, -1, sizeof dp); 
+//     }
+//     int minimumTotal(vector<vector < int>> &triangle, int c = 0, int r = 0)
+//     {
+//         int n = triangle.size();
+//         if (r == n) return 0;
+//         if (dp[r][c] != -1) return dp[r][c];
+
+//         return dp[r][c] = triangle[r][c] + min(minimumTotal(triangle, c, r + 1), minimumTotal(triangle, c + 1, r + 1));
+//     }
+    
+    // Using Tabulation
+    int minimumTotal(vector<vector < int>> &triangle)
     {
         int n = triangle.size();
-        if (r == n) return 0;
-        if (dp[r][c] != -1) return dp[r][c];
-
-        return dp[r][c] = triangle[r][c] + min(minimumTotal(triangle, c, r + 1), minimumTotal(triangle, c + 1, r + 1));
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            int c=triangle[i].size();
+            for(int j=0;j<c;j++)
+            {
+                triangle[i][j]+=min(triangle[i+1][j],triangle[i+1][j+1]);
+            }
+        }
+        return triangle[0][0];
     }
 };
