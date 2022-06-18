6a30d37a0a342cb2dca022e8b91b75b7de2240cb
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-06-18 18:48:11 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (38.84%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,29 +1,30 @@
 class Solution {
-public:    
-    bool isValid(int sum,vector<int>&arr,int m)
+public:
+    bool Check(vector<int>&nums,int mid,int m)
     {
-        int temp=0;
-        for(int i=0;i<arr.size();i++)
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
         {
-            temp+=arr[i];
-            if(temp>sum)
+            sum+=nums[i];
+            if(sum>mid)
             {
-                  temp=arr[i];
+                sum=nums[i];
+                if(sum>mid)return false;
                 m--;
-                if(m<0)return false;
             }
         }
+        if(m<=0)return false;
         return true;
     }
     int splitArray(vector<int>& nums, int m) {
-        int low=*max_element(nums.begin(),nums.end()),high=accumulate(nums.begin(),nums.end(),0);
-        int answer=0;
+        long long low=0,high=1e9;
+        int ans=0;
         while(low<=high)
         {
-            int mid=low+(high-low)/2;
-            if(isValid(mid,nums,m-1))
+            long long mid=low+(high-low)/2;
+            if(Check(nums,mid,m))
             {
-                answer=mid;
+                ans=mid;
                 high=mid-1;
             }
             else
@@ -31,6 +32,6 @@ public:
                 low=mid+1;
             }
         }
-        return answer;
+        return ans;
     }
 };
