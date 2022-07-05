7a53c0f0260055656286d6e39dc2fdd8808b5246
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-07-05 20:20:55 +0530

    Time: 301 ms (22.08%), Space: 94.3 MB (65.62%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.cpp
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
+//     int partitionDisjoint(vector<int>& nums) {
+//         map<int,int>freq;
+//         int n=nums.size();
+//         for(int i=1;i<n;i++)
+//         {
+//             freq[nums[i]]++;
+//         }
+        
+//         int res=1,Max=nums[0];
+//         for(int i=1;i<n;i++)
+//         {
+//             if(Max>freq.begin()->first)
+//             {
+//                 freq[nums[i]]--;
+//                 if(freq[nums[i]]==0)freq.erase(nums[i]);
+//                 res++;
+//                 Max=max(Max,nums[i]);
+//             }
+//         }
+//         return res;
+//     }
     int partitionDisjoint(vector<int>& nums) {
-        map<int,int>freq;
+        int it1=1,Max=nums[0];
         int n=nums.size();
         for(int i=1;i<n;i++)
         {
-            freq[nums[i]]++;
-        }
-        
-        int res=1,Max=nums[0];
-        for(int i=1;i<n;i++)
-        {
-            if(Max>freq.begin()->first)
+            if(Max>nums[i])
             {
-                freq[nums[i]]--;
-                if(freq[nums[i]]==0)freq.erase(nums[i]);
-                res++;
-                Max=max(Max,nums[i]);
+                for(it1;it1<=i;it1++)
+                {
+                    Max=max(nums[it1],Max);
+                }
             }
         }
-        return res;
+        return it1;
     }
 };
