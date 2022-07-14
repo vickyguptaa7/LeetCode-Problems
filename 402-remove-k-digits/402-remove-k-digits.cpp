000ab4555480249903b13cb556f27ac219805369
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-07-14 21:04:24 +0530

    Time: 31 ms (18.10%), Space: 13.8 MB (5.13%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -2,64 +2,32 @@ class Solution {
 public:
     string removeKdigits(string num, int k) {
         stack<char>st;
-        int n=num.length();
-        for(int i=0;i<n;i++)
+        for(int i=0;i<num.size();i++)
         {
-            while(!st.empty()&&k>0&&st.top()>num[i])
+            while(!st.empty()&&st.top()>num[i]&&k)
             {
-                st.pop();
                 k--;
+                st.pop();
             }
             st.push(num[i]);
         }
-        string res="";
-        while(!st.empty())
+        
+        while(k>0)
         {
-            if(k>0)
-            {
-                k--;
-                st.pop();
-                continue;
-            }
-            res=st.top()+res;
+            k--;
             st.pop();
         }
-        while(res[0]=='0')
+        
+        string ans;
+        while(!st.empty())
         {
-            res.erase(0,1);
+            ans+=st.top();
+            st.pop();
         }
-        if(res.size()==0)return "0";
-        return res;
+        
+        reverse(ans.begin(),ans.end());
+        while(!ans.empty()&&ans.front()=='0')ans=ans.substr(1);
+        if(ans.empty())ans="0";
+        return ans;
     }
-//         string removeKdigits(string num, int k) {
-//         for(int i=1;i<num.length();i++)
-//         {
-//             if(num[i-1]>num[i])
-//             {
-//                 num.erase(i-1,1);
-//                 k--;
-//                 if(i>=2)
-//                 i-=2;
-//                 else
-//                     i--;
-//             }
-//             if(k==0)
-//             {
-//                 break;
-//             }
-//         }
-        
-//         num=num.substr(0,num.length()-k);
-        
-//         int iter=0;
-//         while(num[iter]=='0')
-//         {
-//             num.erase(0,1);
-//         }
-//         if(num.length()==0)
-//         {
-//             return "0";
-//         }
-//         return num;
-//     }
 };
