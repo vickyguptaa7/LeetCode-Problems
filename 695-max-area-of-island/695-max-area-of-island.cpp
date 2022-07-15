3847ab5f80a48ad02df1359a5050c767a3c90592
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-07-15 09:42:21 +0530

    Time: 27 ms (65.32%), Space: 24.7 MB (20.70%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,35 +1,57 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,vector<vector<int>>& grid,vector<vector<bool>>& isVisited)
+    vector<int>moveR={0,0,1,-1};
+    vector<int>moveC={1,-1,0,0};
+
+    bool isValid(int r,int c,vector<vector<int>>&grid,
+                vector<vector<int>>&visit)
     {
-        int r=grid.size(),c=grid[0].size();
-        if(row>=r||row<0||col>=c||col<0)return 0;
-        else if(grid[row][col]==0)return 0;
-        else if(isVisited[row][col]==1)return 0;
-        else return 1;
+        int row=grid.size(),col=grid[0].size();
+        
+        // out of bound check 
+        if(r>=row||r<0||c>=col||c<0)
+            return false;
+        
+        // water not land
+        if(grid[r][c]==0)return false;
+        
+        // already visited
+        if(visit[r][c])return false;
+        
+        //land
+        return true;
     }
-    int Helper(int row,int col,vector<vector<int>>& grid,vector<vector<bool>>& isVisited)
+    int helper(int r,int c,vector<vector<int>>&grid,vector<vector<int>>&dp,
+               vector<vector<int>>&visit)
     {
-        if(!isSafe(row,col,grid,isVisited))return 0;
-        isVisited[row][col]=true;
-        int right=Helper(row,col+1,grid,isVisited);
-        int down=Helper(row+1,col,grid,isVisited);
-        int left=Helper(row,col-1,grid,isVisited);
-        int up=Helper(row-1,col,grid,isVisited);
-        return right+down+left+up+1;
+        visit[r][c]=true;
+        if(dp[r][c]!=-1)
+            return dp[r][c];
+        
+        int area=0;
+        
+        for(int i=0;i<4;i++)
+        {
+            int _r=r+moveR[i],_c=c+moveC[i];
+            if(isValid(_r,_c,grid,visit))
+            area+=1+helper(_r,_c,grid,dp,visit);
+        }
+        return dp[r][c]=area;
     }
+        
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int row=grid.size(),col=grid[0].size();
-        vector<vector<bool>>isVisited(row,vector<bool>(col,0));
-        int Max_Area=0;
+        vector<vector<int>>dp(row,vector<int>(col,-1));
+        int ans=0;
+        vector<vector<int>>visit(row,vector<int>(col,false));
         for(int i=0;i<row;i++)
         {
             for(int j=0;j<col;j++)
             {
-                if(grid[i][j]==0||isVisited[i][j]==1)continue;
-                Max_Area=max(Max_Area,Helper(i,j,grid,isVisited));
+                if(grid[i][j]==1)
+                    ans=max(ans,1+helper(i,j,grid,dp,visit));
             }
         }
-        return Max_Area;
+        return ans;
     }
 };
