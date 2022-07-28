bcea461239d2261dacd393a8ab51da9cee5000a1
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-07-28 17:24:12 +0530

    Time: 12 ms (65.55%), Space: 7.3 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return (s.compare(t)==0);
+        if(s.size()!=t.size())return false;
+        vector<int>freq(26,0);
+        for(int i=0;i<s.size();i++)
+        {
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+        for(auto x:freq)
+        {
+            if(x!=0)return false;
+        }
+        return true;
     }
 };
