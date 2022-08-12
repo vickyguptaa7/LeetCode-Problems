b58a83ab01f873363d05e6cc3b5d23d0302ee029
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-08-12 09:13:42 +0530

    Time: 45 ms (54.77%), Space: 23.2 MB (63.86%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,18 +10,20 @@
 
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root)return nullptr;
-        if(root==p||root==q)return root;
+    TreeNode* helper(TreeNode* root, TreeNode* p, TreeNode* q,int Max,int Min)
+    {
+        if(!root||p->val<Min||q->val<Min||p->val>Max||q->val>Max)return nullptr;
         
-        TreeNode*left=lowestCommonAncestor(root->left,p,q);
-
-        TreeNode*right=lowestCommonAncestor(root->right,p,q);
-
-        
-        if((left==p&&right==q)||(right==p&&left==q))return root;
-        if(right)return right;
+        TreeNode*left=helper(root->left,p,q,root->val,Min);
         if(left)return left;
+        TreeNode*right=helper(root->right,p,q,Max,root->val);
+        if(right)return right;
+        
+        if(p->val>Min&&q->val>Min&&p->val<Max&&q->val<Max)return root;
+        
         return nullptr;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return helper(root,p,q,1e9+5,-1e9-5);
     }
 };
