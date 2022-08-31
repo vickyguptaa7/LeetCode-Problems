5f8804c715f6c08dbda41dd61b80817f6c53915c
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-08-31 18:55:07 +0530

    Time: 59 ms (72.04%), Space: 19.4 MB (40.55%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -4,51 +4,51 @@ public:
     vector<int>moveX={0,0,-1,1};
     vector<int>moveY={1,-1,0,0};
     void dfs(int x,int y,vector<vector<int>>&heights,
-             vector<vector<int>>&visit,bool &pacific,bool &atlantic)
-    {
-        if(atlantic&&pacific)
-        {
-            return;
-        }
-        
+             vector<vector<int>>&ocean)
+    {   
         int rows=heights.size(),cols=heights[0].size();
-        visit[x][y]=true;
+        ocean[x][y]=true;
         for(int i=0;i<4;i++)
         {
             int _x=x+moveX[i],_y=y+moveY[i];
             if(_x>=rows||_y>=cols)
             {
-                atlantic=true;
                 continue;
             }
             if(_x<0||_y<0)
             {
-                pacific=true;
                 continue;
             }
-            if(visit[_x][_y]||heights[x][y]<heights[_x][_y])
+            if(ocean[_x][_y]||heights[x][y]>heights[_x][_y])
             {
                 continue;
             }
-            dfs(_x,_y,heights,visit,pacific,atlantic);
+            dfs(_x,_y,heights,ocean);
         }
-        visit[x][y]=false;
     }
     
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         vector<vector<int>>result;
         int rows=heights.size(),cols=heights[0].size();
-        vector<vector<int>>dp(rows+1,vector<int>(cols+1,-1)),visit(rows+1,vector<int>(cols+1,0));
+        
+        vector<vector<int>>pacific(rows,vector<int>(cols,0)),atlantic(rows,vector<int>(cols,0));
+        
+        for(int i=0;i<rows;i++)
+        {
+            dfs(i,0,heights,pacific);
+            dfs(i,cols-1,heights,atlantic);
+        }
+        for(int i=0;i<cols;i++)
+        {
+            dfs(0,i,heights,pacific);
+            dfs(rows-1,i,heights,atlantic);
+        }
+        
         for(int i=0;i<rows;i++)
         {
             for(int j=0;j<cols;j++)
             {
-                bool pacific=false,atlantic=false;
-                dfs(i,j,heights,visit,pacific,atlantic);
-                if(pacific&&atlantic)
-                {
-                    result.push_back({i,j});
-                }
+                if(pacific[i][j]&&atlantic[i][j])result.push_back({i,j});
             }
         }
         return result;
