b4ce97c645728ce543b3a09622dd8b2914831505
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-09-29 15:02:36 +0530

    Time: 201 ms (14.11%), Space: 34.1 MB (37.52%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int,int>>pque;
+        for(auto num:arr)
+        {
+            pque.push({abs(x-num),num});
+            if(pque.size()>k)pque.pop();
+        }
         vector<int>res;
-        int it1=lower_bound(arr.begin(),arr.end(),x)-arr.begin();
-        int it2=it1;
-        it1--;
-        while(it1>=0&&it2<arr.size())
+        while(!pque.empty())
         {
-            if(k==0)break;
-            if(abs(arr[it1]-x)>abs(arr[it2]-x))
-            {
-                res.push_back(arr[it2]);
-                it2++;
-            }
-            else
-            {
-                res.push_back(arr[it1]);
-                it1--;
-            }
-            k--;
-        }
-        while(k>0&&it1>=0)
-        {
-            k--;
-            res.push_back(arr[it1]);
-            it1--;
-        }
-        while(k>0&&it2<arr.size())
-        {
-            k--;
-            res.push_back(arr[it2]);
-            it2++;
+            res.push_back(pque.top().second);
+            pque.pop();
         }
         sort(res.begin(),res.end());
         return res;
