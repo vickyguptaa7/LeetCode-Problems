48c3a0f67858ab14b5cba72902867c6909ec04e7
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-10-02 07:52:26 +0530

    Time: 238 ms (5.19%), Space: 9.9 MB (19.81%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -22,7 +22,22 @@ public:
         return dp[target][n]=ways;
     }
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>>dp(target+1,vector<int>(n+1,-1));
-        return helper(n,k,target,dp);
+        vector<vector<int>>dp(target+1,vector<int>(n+1,0));
+        dp[0][0]=1;
+        int mod=1e9+7;
+        for(int i=1;i<=target;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                for(int l=1;l<=k;l++)
+                {
+                    if(l>i)
+                        continue;
+                    dp[i][j]+=dp[i-l][j-1];
+                    dp[i][j]%=mod;
+                }
+            }
+        }
+        return dp[target][n];
     }
 };
