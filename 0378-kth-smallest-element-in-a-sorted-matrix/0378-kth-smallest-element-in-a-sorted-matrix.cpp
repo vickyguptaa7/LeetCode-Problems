ba140170f67fb9eaf6366fa7982b674512c315d2
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-10-21 17:44:03 +0530

    Time: 50 ms (73.36%), Space: 25.1 MB (5.06%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,28 +1,68 @@
-
-typedef pair<int,pair<int,int>> ppi; 
-
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<ppi,vector<ppi>,greater<ppi>>pque;
-        int n=matrix.size();
-        for(int i=0;i<n;i++)
+    
+    int isValidCount(vector<vector<int>>& matrix, int value)
+    {
+        int cnt=0;
+        for(auto x:matrix)
         {
-            pque.push({matrix[i][0],{i,0}});
-        }
-        k--;
-        while(k)
-        {
-            auto curr=pque.top();
-            int _x=curr.second.first;
-            int _y=curr.second.second;
-            pque.pop();
-            if(_y!=n-1)
+            for(auto y:x)
             {
-                pque.push({matrix[_x][_y+1],{_x,_y+1}});
+                if(y>value)
+                    break;
+                cnt++;
             }
-            k--;
         }
-        return pque.top().first;
+        return cnt;
+    }
+    
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n=matrix.size();
+        int low=matrix[0][0],high=matrix[n-1][n-1];
+        int ans=0;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(isValidCount(matrix,mid)>=k)
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return ans;
     }
 };
+
+
+
+// typedef pair<int,pair<int,int>> ppi; 
+
+// class Solution {
+// public:
+//     int kthSmallest(vector<vector<int>>& matrix, int k) {
+//         priority_queue<ppi,vector<ppi>,greater<ppi>>pque;
+//         int n=matrix.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             pque.push({matrix[i][0],{i,0}});
+//         }
+//         k--;
+//         while(k)
+//         {
+//             auto curr=pque.top();
+//             int _x=curr.second.first;
+//             int _y=curr.second.second;
+//             pque.pop();
+//             if(_y!=n-1)
+//             {
+//                 pque.push({matrix[_x][_y+1],{_x,_y+1}});
+//             }
+//             k--;
+//         }
+//         return pque.top().first;
+//     }
+// };
