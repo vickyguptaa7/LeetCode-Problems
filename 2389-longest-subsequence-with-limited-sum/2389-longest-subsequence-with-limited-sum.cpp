d96cc1606412e06f2f9b84250e5c2f2d10559568
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2022-12-25 09:41:13 +0530

    Time: 18 ms (93.30%), Space: 15.7 MB (5.00%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
-        vector<int>result;
+        map<int,int>msize;
         sort(nums.begin(),nums.end());
+        vector<int>squery=queries;
+        sort(squery.begin(),squery.end());
+        int iter=0,sum=0;
+        for(int i=0;i<nums.size()&&iter<squery.size();i++)
+        {
+            if(squery[iter]>=sum+nums[i])
+            {
+                sum+=nums[i];
+            }
+            else 
+            {
+                i--;
+                msize[squery[iter++]]=i+1;
+            }
+        }
+        while(iter<squery.size())
+        {
+            msize[squery[iter++]]=nums.size();
+        }
+        vector<int>res;
         for(auto x:queries)
         {
-            int sum=x,cntr=0;
-            for(auto y:nums)
-            {
-                if(sum>=y)
-                {
-                    sum-=y;
-                    cntr++;
-                }
-                else 
-                    break;
-            }
-            result.push_back(cntr);
+            res.push_back(msize[x]);
         }
-        return result;
+        return res;
     }
 };
