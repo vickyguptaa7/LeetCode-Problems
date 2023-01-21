f2b38a58a47b99f5e74fe3b50836e3ca31e6d7c3
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-01-21 17:37:59 +0530

    Time: 13 ms (10.28%), Space: 9 MB (5.56%) - LeetHub

--- 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
     
-    void helper(int n,int num,int prev,int &cntr)
+    int helper(int strt,string &binary,bool restrict,bool isPrevOne,
+                vector<vector<vector<int>>>&dp)
     {
-        if(n>=num*2)
+        if(strt==binary.size())
         {
-            cntr++;
-            helper(n,num*2,0,cntr);
+            return 1;
         }
-        if(n>=num*2+1&&prev==0)
-        {
-            cntr++;
-            helper(n,num*2+1,1,cntr);
-        }
+        if(dp[strt][restrict][isPrevOne]!=-1)
+            return dp[strt][restrict][isPrevOne];
         
+        int end=(restrict)?binary[strt]-'0':2;
+        int res=0;
+        for(int i=0;i<end;i++)
+        {
+            if(i==1&&isPrevOne)
+                continue;
+            res+=helper(strt+1,binary,false,i==1,dp);
+        }
+        if(restrict&&(!isPrevOne||end!=1))
+            res+=helper(strt+1,binary,true,end==1,dp);
+        
+        return dp[strt][restrict][isPrevOne]=res;
+    }
+    string decToBinary(int n)
+    {
+        string s;
+        while(n)
+        {
+            s+=(n&1)?"1":"0";
+            n/=2;
+        }
+        reverse(s.begin(),s.end());
+        cout<<s<<"\n";
+        return s;
     }
     int findIntegers(int n) {
-        int cntr=2;// for 0 and 1
-        helper(n,1,1,cntr);
-        return cntr;
+        string binary=decToBinary(n);
+        vector<vector<vector<int>>>dp(binary.size(),
+                        vector<vector<int>>(2,vector<int>(2,-1)));
+        return helper(0,binary,true,false,dp);
     }
 };
