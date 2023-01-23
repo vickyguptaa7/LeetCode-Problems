047927a3e742939b0ac7c1334696c4ec14bf98be
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-01-23 17:11:10 +0530

    Time: 0 ms (100.00%), Space: 6 MB (16.68%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkPal(string s)
+    {
+        int n=s.size();
+        for(int i=0;i<n/2;i++)
+        {
+            if(s[i]!=s[n-1-i])return false;
+        }
+        return true;
+    }
+    bool isStrictlyPalindromic(int n) {
+        for(int i=2;i<=n-2;i++)
+        {
+            int temp=n;
+            string s;
+            while(temp)
+            {
+                s+=to_string(temp%i);
+                temp/=i;
+            }
+            if(!checkPal(s))return false;
+        }
+        return true;
+    }
+};
