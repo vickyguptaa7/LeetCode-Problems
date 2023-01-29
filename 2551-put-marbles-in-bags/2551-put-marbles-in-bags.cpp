39512f08ca5a139f4da5e3cab0536443eb10c5cc
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-01-29 11:31:21 +0530

    Time: 410 ms (20.00%), Space: 65.3 MB (80.00%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -1,21 +1,26 @@
-class Solution {
-public:
-    long long putMarbles(vector<int>& weights, int k) {
-        priority_queue<int>pque1;
-        priority_queue<int,vector<int>,greater<int>>pque2;
-        for(int i=1;i<weights.size();i++)
+class Solution
+{
+    public:
+        long long putMarbles(vector<int> &weights, int k)
         {
-            pque1.push(weights[i-1]+weights[i]);
-            pque2.push(weights[i-1]+weights[i]);
+            priority_queue<int> pque1;
+            priority_queue<int, vector < int>, greater < int>> pque2;
+            for (int i = 1; i < weights.size(); i++)
+            {
+                pque1.push(weights[i - 1] + weights[i]);
+                pque2.push(weights[i - 1] + weights[i]);
+                if (pque1.size() == k) {
+                    pque1.pop();
+                    pque2.pop();
+                }
+            }
+            long long ans = 0;
+            while (!pque1.empty())
+            {
+                ans += pque2.top() - pque1.top() + 0ll;
+                pque1.pop();
+                pque2.pop();
+            }
+            return ans;
         }
-        long long ans=0;
-        k--;
-        while(k--)
-        {
-            ans+=pque1.top()-pque2.top()+0ll;
-            pque1.pop();
-            pque2.pop();
-        }
-        return ans;
-    }
 };
