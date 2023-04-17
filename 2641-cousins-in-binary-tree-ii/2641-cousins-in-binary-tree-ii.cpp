7ecbaf595237434db25c6e3dc49107d546471a24
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-04-17 10:14:59 +0530

    Time: 909 ms (25.00%), Space: 484.8 MB (25.00%) - LeetHub

---
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        
+        queue<pair<TreeNode*,TreeNode*>>que;
+        que.push({root,nullptr});
+        int psum=root->val;
+        while(!que.empty())
+        {
+            int size=que.size();
+            int sum=0;
+            map<TreeNode*,int>childSum;
+            queue<pair<TreeNode*,TreeNode*>>tempQue;
+            while(size--)
+            {
+                auto curr=que.front();
+                que.pop();
+                tempQue.push(curr);
+                sum+=curr.first->val;
+                childSum[curr.second]+=curr.first->val;
+                if(curr.first->left)
+                {
+                    que.push({curr.first->left,curr.first});
+                }
+                if(curr.first->right)
+                {
+                    que.push({curr.first->right,curr.first});
+                }
+            }
+            size=tempQue.size();
+            while(size--)
+            {
+                auto curr=tempQue.front();
+                tempQue.pop();
+                curr.first->val=sum-childSum[curr.second];
+            }
+        }
+        return root;
+    }
+};
