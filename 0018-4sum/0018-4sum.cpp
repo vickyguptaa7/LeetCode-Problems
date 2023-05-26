14be01f0bce3858e91f75b9800240a09d81b5a1a
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-05-26 20:45:33 +0530

    Time: 100 ms (51.35%), Space: 13 MB (94.87%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -7,18 +7,20 @@ public:
         set<vector<int>>oset;
         for(int i=0;i<nums.size();i++)
         {
+            if(i>0&&nums[i]==nums[i-1])continue;
             for(int j=i+1;j<nums.size();j++)
             {
+                if(j>i+1&&nums[j]==nums[j-1])
+                    continue;
                 int left=j+1,right=n-1;
                 long long curr=target-(long long)(nums[i]+nums[j]);
                 while(left<right)
                 {
-                    if(curr-(nums[left]+nums[right])==0
-                       &&!oset.count({nums[i],nums[j],nums[left],nums[right]}))
+                    if(curr-(nums[left]+nums[right])==0)
                     {
-                        oset.insert({nums[i],nums[j],nums[left],nums[right]});
                         quad.push_back({nums[i],nums[j],nums[left],nums[right]});
                         left++;
+                        while(left<right&&nums[left]==nums[left-1])left++;
                     }
                     else if(curr-(nums[left]+nums[right])<0)
                     {
