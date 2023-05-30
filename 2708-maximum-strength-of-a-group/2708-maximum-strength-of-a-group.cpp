0d28f67f4cf6fa86fa834751139d11fa2e252bd1
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-05-30 11:00:23 +0530

    Time: 43 ms (20.32%), Space: 46.6 MB (38.37%) - LeetHub

---
+++ 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    public:
+
+        void helper(int indx, vector<int> &nums,long long ans,long long &result)
+        {
+            result=max(result,ans);
+            if (indx == nums.size())
+                return;
+            helper(indx+1,nums,ans,result);
+            if(ans==-1e18)
+            {
+                ans=1;
+            }
+            helper(indx+1,nums,ans*nums[indx],result);
+        }
+
+    long long maxStrength(vector<int> &nums)
+    {
+        int mx=*max_element(nums.begin(),nums.end());
+        long long res=-1e18;
+        helper(0,nums,-1e18,res);
+        return res;
+    }
+};
