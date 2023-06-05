540a45d048c3e26b482dc2f92dcf6686b48d0354
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-06-05 22:38:00 +0530

    Time: 1251 ms (5.04%), Space: 20.9 MB (70.20%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int n=matrix.size(),m=matrix[0].size();
+        vector<vector<int>>preSum(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                bool isOne=(matrix[i-1][j-1]=='1')?1:0;
+                preSum[i][j]+=isOne+preSum[i-1][j]+preSum[i][j-1]-preSum[i-1][j-1];
+            }
+        }
+        int mxSide=min(n,m);
+        int mxSquare=0;
+        for(int side=1;side<=mxSide;side++)
+        {
+            for(int i=side-1;i<n;i++)
+            {
+                for(int j=side-1;j<m;j++)
+                {
+                    int sum=preSum[i+1][j+1]-
+                        preSum[i-side+1][j+1]-preSum[i+1][j-side+1]+preSum[i-side+1][j-side+1];
+                    if(side*side==sum)
+                        mxSquare=max(mxSquare,sum);
+                }
+            }
+        }
+        return mxSquare;
+    }
+};
