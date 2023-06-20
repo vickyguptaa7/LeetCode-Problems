44612907c04aae904a1f0622e8cf2a4be204765c
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-06-20 09:50:46 +0530

    Time: 262 ms (48.49%), Space: 129.9 MB (79.40%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -3,29 +3,33 @@ public:
     vector<int> getAverages(vector<int>& nums, int k) {
         int n=nums.size();
         
-        // prefix sum
-        vector<long long>preSum(n,0);
-        preSum[0]=nums[0];
-        for(int i=1;i<n;i++)
-        {
-            preSum[i]=preSum[i-1]+nums[i];
-        }
+        vector<int>average(n,-1);
+        if(2*k+1>n)
+            return average;
         
-        vector<int>average(n,0);
-        for(int i=0;i<n;i++)
+        long long sum=0;
+        
+        // for left most k ans will be -1
+        for(int i=0;i<k;i++)
         {
-            // in left or right less than k so -1 
-            if(i-k<0||i+k>=n)
-            {
-                average[i]=-1;
-                continue;
-            }
-            
-            long long sum=preSum[i+k];
-            if(i-k!=0)
-                sum-=preSum[i-k-1];
-            sum/=(2*k+1ll);
-            average[i]=sum;
+            sum+=nums[i];
+            average[i]=-1;
+        }
+        for(int i=k;i<n&&i<2*k+1;i++)
+        {
+            sum+=nums[i];
+        }
+        for(int i=k;i<n-k;i++)
+        {
+            average[i]=sum/(2*k+1ll);
+            sum-=nums[i-k];
+            if(i+k+1<n)
+                sum+=nums[i+k+1];
+        }
+        // for right most k ans will be -1
+        for(int i=n-k;i<n;i++)
+        {
+            average[i]=-1;
         }
         return average;
     }
