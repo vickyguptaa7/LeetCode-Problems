459a4003b915c35231194275a8343b8c32519bed
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-07-13 09:50:58 +0530

    Time: 30 ms (84.05%), Space: 12.5 MB (73.41%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-        int maxPointOnLine=1;
+        int mxPoints=1;
         for(int i=0;i<points.size();i++)
         {
             map<pair<int,int>,int>freq;
             for(int j=i+1;j<points.size();j++)
             {
-                int x=points[i][0]-points[j][0];
-                int y=points[i][1]-points[j][1];
-                int gcd=__gcd(x,y);
-                freq[{x/gcd,y/gcd}]++;
+                int _x=points[i][0]-points[j][0];
+                int _y=points[i][1]-points[j][1];
+                int gcd=__gcd(_x,_y);
+                freq[{_x/gcd,_y/gcd}]++;
             }
             for(auto x:freq)
-            {
-                maxPointOnLine=max(maxPointOnLine,x.second+1);
-            }
+                mxPoints=max(mxPoints,x.second+1);
         }
-        return maxPointOnLine;
+        return mxPoints;
     }
 };
