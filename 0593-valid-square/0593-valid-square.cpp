f570f6934e14c08e505d36aad40c564f35ad8cb4
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-07-13 10:12:41 +0530

    Time: 18 ms (10.49%), Space: 26.9 MB (34.18%) - LeetHub

--- 0593-valid-square/0593-valid-square.cpp
+++ 0593-valid-square/0593-valid-square.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
-        vector<pair<int,int>>cord;
-        cord.push_back({p1[0],p1[1]});
-        cord.push_back({p2[0],p2[1]});
-        cord.push_back({p3[0],p3[1]});
-        cord.push_back({p4[0],p4[1]});
-        map<long double,int>freq;
-        for(auto [x,y]:cord)
+        vector<pair<int,int>>coord;
+        coord.push_back({p1[0],p1[1]});
+        coord.push_back({p2[0],p2[1]});
+        coord.push_back({p3[0],p3[1]});
+        coord.push_back({p4[0],p4[1]});
+        map<int,int>freq;
+        for(int i=0;i<4;i++)
         {
-            for(auto [_x,_y]:cord)
+            for(int j=0;j<4;j++)
             {
-                if(_x==x&&_y==y)
-                    continue;
-                freq[(x-_x)*(x-_x)+(y-_y)*(y-_y)]++;
+                if(i==j||coord[i]==coord[j])continue;
+                int x=coord[i].first-coord[j].first;
+                int y=coord[i].second-coord[j].second;
+                freq[x*x+y*y]++;
             }
         }
-        if(!freq.empty()&&freq.begin()->second==8&&freq.rbegin()->second==4)
+        if(freq.size()==2&&freq.begin()->second==8&&freq.rbegin()->second==4)
             return true;
         return false;
     }
