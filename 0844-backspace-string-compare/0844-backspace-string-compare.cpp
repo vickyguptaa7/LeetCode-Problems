6f63ad6ecbde1e3a81de84fb0960ec83df84d161
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-07-18 10:09:49 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (91.18%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -20,6 +20,75 @@ public:
     }
     
     bool backspaceCompare(string s, string t) {
-        return backSpace(s)==backSpace(t);
+        reverse(s.begin(),s.end());
+        reverse(t.begin(),t.end());
+        int it1=0,it2=0;
+        while(it1<s.size()&&it2<t.size())
+        {
+            if(s[it1]!='#'&&t[it2]!='#')
+            {
+                
+                if(s[it1]!=t[it2])
+                    return false;
+                it1++;
+                it2++;
+                continue;
+            }
+            int cntr=1;
+            while(it1<s.size())
+            {
+                cntr+=(s[it1]=='#')?1:-1;
+                if(cntr>0)
+                {
+                    it1++;
+                    continue;
+                }
+                else 
+                    break;
+                    
+            }
+            cntr=1;
+            while(it2<t.size())
+            {
+                cntr+=(t[it2]=='#')?1:-1;
+                if(cntr>0)
+                {
+                    it2++;
+                    continue;
+                }
+                else 
+                    break;
+                
+            }
+        }
+        int cntr=1;
+            while(it1<s.size())
+            {
+                cntr+=(s[it1]=='#')?1:-1;
+                if(cntr>0)
+                {
+                    it1++;
+                    continue;
+                }
+                else 
+                    break;
+                    
+            }
+            cntr=1;
+            while(it2<t.size())
+            {
+                cntr+=(t[it2]=='#')?1:-1;
+                if(cntr>0)
+                {
+                    it2++;
+                    continue;
+                }
+                else 
+                    break;
+                
+            }
+        if(it1<s.size()||it2<t.size())
+            return false;
+        return true;
     }
 };
