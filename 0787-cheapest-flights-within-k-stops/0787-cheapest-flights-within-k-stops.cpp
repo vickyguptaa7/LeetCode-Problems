16b6d7391fa00c35382000a6404f014b995ceafd
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-08-01 16:02:34 +0530

    Time: 26 ms (62.83%), Space: 15.2 MB (15.05%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
-    typedef pair<int,pair<int,int>> ppi;
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         
         vector<pair<int,int>>list[n];
-        
-        for(int i=0;i<flights.size();i++)
+        for(auto edge:flights)
         {
-            list[flights[i][0]].push_back
-                ({flights[i][1],flights[i][2]});
+            list[edge[0]].push_back({edge[1],edge[2]});
         }
         
-        priority_queue<ppi,vector<ppi>,greater<ppi>>pq;
-        pq.push({0,{src,0}});
-        
-        vector<int>stop(n,1e9);
-        
-        while(pq.size())
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>
+            pque;
+        pque.push({0,0,src});
+        vector<int>stops(n,1e9);
+        stops[src]=0;
+        while(!pque.empty())
         {
-            int node=pq.top().second.first;
-            int weight=pq.top().first;
-            int radial_dist=pq.top().second.second;
-            pq.pop();
-            if(radial_dist>stop[node]||radial_dist>k+1)
-            {
+            int cost=pque.top()[0];
+            int stop=pque.top()[1];
+            int curr=pque.top()[2];
+            
+            pque.pop();
+            if(curr==dst)
+                return cost;
+            
+            if(stop>k||stops[curr]<stop)
                 continue;
-            }
-            stop[node]=radial_dist;
-            if(dst==node)
+            stops[curr]=stop;
+            for(auto child:list[curr])
             {
-                return weight;
-            }
-            for(auto x:list[node])
-            {
-                pq.push({weight+x.second,{x.first,radial_dist+1}});
+                pque.push({cost+child.second,stop+1,child.first});
             }
         }
         return -1;
+        
     }
 };
