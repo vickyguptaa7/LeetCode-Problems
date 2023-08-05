fdcb46cd22dff055564ec2baa9c8ccebce208aab
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-08-05 23:33:55 +0530

    Time: 16 ms (81.26%), Space: 8.9 MB (74.79%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    
-    vector<int>moveX={0,0,-1,1},moveY={1,-1,0,0};
-    int helper(int x,int y,int time,vector<vector<int>>&grid,
-               vector<vector<vector<int>>>&dp,vector<vector<bool>>&visited)
-    {
-        int n=grid.size();
-        if(x==n-1&&y==n-1)
-        {
-            return time+max(0,grid[x][y]-time);
-        }
-        
-        if(dp[x][y][time]!=-1)
-            return dp[x][y][time];
-        visited[x][y]=true;
-        int mtime=1e9;
-        for(int i=0;i<4;i++)
-        {
-            int _x=x+moveX[i],_y=moveY[i]+y;
-            if(_x>=n||_y>=n||_y<0||_x<0||visited[_x][_y])
-                continue;
-            mtime=min(mtime,helper(_x,_y,time+max(0,grid[x][y]-time),grid
-                                   ,dp,visited));
-        }
-        visited[x][y]=false;
-        return dp[x][y][time]=mtime;
-    }
     int swimInWater(vector<vector<int>>& grid) {
-        int n=grid.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(
-        n,vector<int>(n*n+1,-1)));
-        vector<vector<bool>>visited(n,vector<bool>(n,false));
-        return helper(0,0,0,grid,dp,visited);
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<int>>time(n,vector<int>(m,1e9));
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,
+        greater<pair<int,pair<int,int>>>>pque;
+        
+        time[0][0]=grid[0][0];
+        pque.push({grid[0][0],{0,0}});
+        vector<int>moveX={0,0,-1,1},moveY={1,-1,0,0};
+        while(!pque.empty())
+        {
+            int cost=pque.top().first;
+            int x=pque.top().second.first;
+            int y=pque.top().second.second;
+            pque.pop();
+            if(x==n-1&&y==m-1)
+                return cost;
+            for(int i=0;i<4;i++)
+            {
+                int _x=x+moveX[i],_y=y+moveY[i];
+                if(_x>=n||_y>=m||_x<0||_y<0)
+                    continue;
+                if(time[_x][_y]>max(cost,grid[_x][_y]))
+                {
+                    time[_x][_y]=max(cost,grid[_x][_y]);
+                    pque.push({time[_x][_y],{_x,_y}});
+                }
+            }
+        }
+        return -1;
     }
 };
