3392c751a152d12a3caca388445649fa208546bf
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-08-09 19:26:56 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (43.46%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    int helper(int indx,string&s,vector<int>&dp)
+    {
+        if(indx==s.size())
+            return 1;
+        
+        if(s[indx]=='0')
+            return 0;
+        
+        if(dp[indx]!=-1)
+            return dp[indx];
+        
+        int ways=helper(indx+1,s,dp);
+        if(indx+1!=s.size()&&stoi(s.substr(indx,2))<=26)
+        {
+            ways+=helper(indx+2,s,dp);
+        }
+        return dp[indx]=ways;
+    }
+    
+    int numDecodings(string s) {
+        vector<int>dp(s.size(),-1);
+        return helper(0,s,dp);
+    }
+};
