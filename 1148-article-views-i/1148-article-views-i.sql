c826786dcf518ee5c8345e698b4885305ecc29a1
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-09-22 09:59:58 +0530

    Time: 852 ms (69.13%), Space: 0B (100.00%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,4 +1,3 @@
 # Write your MySQL query statement below
-SELECT DISTINCT author_id as id FROM Views as v1 
-WHERE v1.author_id in ( SELECT DISTINCT viewer_id FROM Views as v2 WHERE v1.author_id=v2.author_id )
-ORDER BY author_id;
+SELECT DISTINCT author_id as id FROM Views
+WHERE author_id=viewer_id ORDER BY author_id;
