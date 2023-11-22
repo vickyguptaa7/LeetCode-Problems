a0daa4d7583e8bc1664fad0fbe6cae033fd9f7cb
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2023-11-22 21:59:58 +0530

    Time: 255 ms (25.85%), Space: 107.3 MB (20.51%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     
-    void helper(int x,int y,vector<vector<int>>&nums,vector<int>&result,int size)
-    {
-        while(x>=0&&y<size)
-        {
-            if(nums[x].size()>y)
-                result.push_back(nums[x][y]);
-            x--;
-            y++;
-        }
-    }
-    
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
-        vector<pair<long long,int>>dummy;
-        long long cntr=1;
+        unordered_map<int,vector<int>>mmp;
+        int size=nums.size();
         for(int i=0;i<nums.size();i++)
         {
-            long long strt=i+2,h=cntr;
             for(int j=0;j<nums[i].size();j++)
             {
-                dummy.push_back({h,nums[i][j]});
-                h+=strt;
-                strt++;
+                size=max(size,i+j);
+                mmp[i+j].push_back(nums[i][j]);
             }
-            cntr+=i+1;
         }
-        sort(dummy.begin(),dummy.end());
         vector<int>res;
-        for(auto d:dummy)res.push_back(d.second);
+        for(int i=0;i<=size;i++)
+        {
+            if(!mmp.count(i))continue;
+            vector<int>temp=mmp[i];
+            int len=temp.size();
+            for(int i=len-1;i>=0;i--)
+            {
+                res.push_back(temp[i]);
+            }
+        }
         return res;
     }
 };
