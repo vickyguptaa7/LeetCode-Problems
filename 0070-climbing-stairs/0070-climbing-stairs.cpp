0399be496a7c8890bffd9dc232ffb9f8b5c3dd16
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2024-01-18 10:55:43 +0530

    Time: 2 ms (45.07%), Space: 6.2 MB (88.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int a=1,b=1;
+        int curr=1,prev=1;
         for(int i=2;i<=n;i++)
         {
-            int c=a+b;
-            a=b;
-            b=c;
+            int next=curr+prev;
+            prev=curr;
+            curr=next;
         }
-        return b;
+        return curr;
     }
 };
