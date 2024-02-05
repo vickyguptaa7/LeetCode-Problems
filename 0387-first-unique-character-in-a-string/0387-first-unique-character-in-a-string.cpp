b1c6ba4276daf1c9653daff8e0318cf11877c4f2
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2024-02-05 10:11:56 +0530

    Time: 16 ms (95.41%), Space: 11.7 MB (31.20%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int freq[26]={0};
+        for(auto &ch:s)freq[ch-'a']++;
+        for(int i=0;i<s.size();i++)
+            if(freq[s[i]-'a']==1)
+                return i;
+        
+        return -1;
+    }
+};
