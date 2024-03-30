388f9f49a942778ae1c49d9de8b81c6666392d07
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2024-03-30 23:04:00 +0530

    Time: 123 ms (26.26%), Space: 49.8 MB (34.20%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -3,7 +3,7 @@ public:
     int helper(vector<int>& nums, int k)
     {
         int tail=0,head=-1;
-        map<int,int>diff;
+        unordered_map<int,int>diff;
         int res=0,n=nums.size();
         while(head<n)
         {
