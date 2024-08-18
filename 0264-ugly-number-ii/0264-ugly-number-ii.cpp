0185c24e5a469904b5c4e4d8509d055e5b711c93
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2024-08-18 10:38:14 +0530

    Time: 71 ms (31.88%), Space: 31.8 MB (24.77%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     int nthUglyNumber(int n) {
         set<long long>pque;
         pque.insert(1);
@@ -14,4 +15,6 @@ public:
         }
         return *pque.begin();
     }
+    // 25 -> 2-12 3-8 5-5 6-4 10-2 15-1 30-0
+    // 25 -> 1 2 3 4 5 6 8 9 10 12 15 16 18 20 24 25 
 };
