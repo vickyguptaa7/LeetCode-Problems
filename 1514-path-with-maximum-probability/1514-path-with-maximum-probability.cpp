f9c31d21afabe6b0184e98bf79c0ad1fdeb22cd4
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2024-08-27 11:58:04 +0530

    Time: 115 ms (92.90%), Space: 69.9 MB (71.19%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+    
+    double helper(int src,int end,vector<pair<int,double>>list[],vector<int>&visit)
+    {
+        priority_queue<pair<double,int>>pque;
+        pque.push({1,src});
+        while(!pque.empty())
+        {
+            int node=pque.top().second;
+            double cost=pque.top().first;
+            visit[node]=true;
+            pque.pop();
+            if(node==end)
+                return cost;
+            for(auto curr:list[node])
+            {
+                if(visit[curr.first])continue;
+                pque.push({curr.second*cost,curr.first});
+            }
+        }
+        return 0;
+    }
+    
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
         vector<pair<int,double>>list[n];
         for(int i=0;i<edges.size();i++)
         {
             list[edges[i][0]].push_back({edges[i][1],succProb[i]});
             list[edges[i][1]].push_back({edges[i][0],succProb[i]});
         }
-        priority_queue<pair<double,int>>pque;
-        pque.push({1,start});
-        vector<double>prob(n,0);
-        prob[start]=1;
-        while(!pque.empty())
-        {
-            auto curr=pque.top();
-            pque.pop();
-            // cout<<curr.second<<"\n";
-            for(auto child:list[curr.second])
-            {
-                // cout<<prob[child.first]<<" "<<curr.first*child.second<<"\n";
-                if(prob[child.first]<curr.first*child.second)
-                {
-                    prob[child.first]=curr.first*child.second;
-                    pque.push({prob[child.first],child.first});
-                }
-            }
-        }
-        return prob[end];
+        vector<int>visit(n,0);
+        return helper(start_node,end_node,list,visit);
     }
 };
