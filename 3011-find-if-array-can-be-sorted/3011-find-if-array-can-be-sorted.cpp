aeb2f72250be35245360eed2d57861498eb9a492
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2024-11-06 10:19:49 +0530

    Time: 0 ms (100.00%), Space: 31.5 MB (46.54%) - LeetHub

---
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    bool canSortArray(vector<int>& nums) {
+        vector<int>group(nums.size(),0);
+        int grp=1;
+        int mn=nums[0],mx=nums[0];
+        int pmn=0,pmx=0;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(__builtin_popcount(nums[i-1])!=__builtin_popcount(nums[i]))
+            {
+                if(pmx>mn)
+                {
+                    return false;
+                }
+                pmn=mn;
+                pmx=mx;
+                mn=nums[i];
+                mx=nums[i];
+            }
+            mx=max(mx,nums[i]);
+            mn=min(mn,nums[i]);
+        }
+        return pmx>mn?false:true;
+    }
+};
