395e3b52638c1a0ac1f3fb24f6a6cea02d211fea
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2025-01-23 12:31:28 +0530

    Time: 1 ms (64.83%), Space: 26.5 MB (81.36%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
 
-    int dfs(int src,int par,map<int,set<int>>&list,map<int,int>&visit)
-    {
-        if(visit[src])
-            return 0;
-        int count=1;
-        visit[src]=1;
-        for(auto child:list[src])
-        {
-            if(child==par||visit[child])continue;
-            count+=dfs(child,src,list,visit);
-        }
-        return count;
-    }
-
     int countServers(vector<vector<int>>& grid) {
-        map<int,set<int>>list;
         int n=grid.size(),m=grid[0].size();
+        vector<int>rows(m,0),cols(n,0);
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(!grid[i][j])continue;
-                int u=i*m+j;
-                for(int k=j+1;k<m;k++)
-                {
-                    if(!grid[i][k])continue;
-                    int v=i*m+k;
-                    list[u].insert(v);
-                }
+                rows[j]++;
             }
         }
         for(int i=0;i<m;i++)
@@ -37,20 +17,17 @@ public:
             for(int j=0;j<n;j++)
             {
                 if(!grid[j][i])continue;
-                int u=j*m+i;
-                for(int k=j+1;k<n;k++)
-                {
-                    if(!grid[k][i])continue;
-                    int v=k*m+i;
-                    list[u].insert(v);
-                }
+                cols[j]++;
             }
         }
         int count=0;
-        map<int,int>visit;
-        for(auto x:list)
+        for(int i=0;i<n;i++)
         {
-            count+=dfs(x.first,-1,list,visit);
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]&&(rows[j]>1||cols[i]>1))
+                    count++;
+            }
         }
         return count;
     }
