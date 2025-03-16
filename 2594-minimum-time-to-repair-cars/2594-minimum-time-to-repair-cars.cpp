f7cf0bca4b643944e76383267ed9bd352ec6a2e5
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2025-03-16 21:40:00 +0530

    Time: 33 ms (68.36%), Space: 59.4 MB (45.76%) - LeetHub

--- 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.cpp
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    
-    bool isPossible(long long time,vector<int>&ranks,int cars)
+
+    bool check(vector<int>&ranks,int cars,long long time)
     {
         for(int i=0;i<ranks.size();i++)
         {
-            int canRepairCars=sqrtl((time/ranks[i]));
-            cars-=canRepairCars;
+            long long r=time/ranks[i];
+            cars-=(int)sqrt(r);
             if(cars<=0)
                 return true;
         }
         return false;
     }
-    
+
     long long repairCars(vector<int>& ranks, int cars) {
-        long long lowTime=1,highTime=1e15,minTime=1;
-        while(lowTime<=highTime)
+        long long low=1,high=1e18,ans=0;
+        while(low<=high)
         {
-            long long midTime=lowTime+(highTime-lowTime)/2;
-            if(isPossible(midTime,ranks,cars))
+            long long mid=low+(high-low)/2;
+            if(check(ranks,cars,mid))
             {
-                minTime=midTime;
-                highTime=midTime-1;
+                ans=mid;
+                high=mid-1;
             }
             else
             {
-                lowTime=midTime+1;
+                low=mid+1;
             }
         }
-        return minTime;
+        return ans;
     }
 };
