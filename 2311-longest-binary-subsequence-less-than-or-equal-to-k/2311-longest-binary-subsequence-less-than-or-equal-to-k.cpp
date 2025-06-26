b7d0ba6f2f4665d307f129a50157ed5a2c0ae2f2
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2025-06-26 22:32:59 +0530

    Time: 326 ms (7.94%), Space: 268.5 MB (6.35%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    
-    int helper(int it,int cnt,string &s,int k,vector<vector<int>>&dp)
+
+    int helper(int lvl,int cnt,string& s,int k,vector<vector<int>>&dp)
     {
-        if(s.size()<=it)
-        {
+        if(s.size()<=lvl) {
             return 0;
         }
-        if(dp[it][cnt]!=-1)return dp[it][cnt];
-        if(s[it]=='0')
-        {
-            return dp[it][cnt]=max(helper(it+1,cnt+1,s,k,dp)+1,helper(it+1,cnt,s,k,dp));
+        if(dp[lvl][cnt]!=-1) {
+            return dp[lvl][cnt];
         }
-        else
-        {
+        if(s[lvl]=='0') {
+            return dp[lvl][cnt]=max(helper(lvl+1,cnt,s,k,dp),1+helper(lvl+1,cnt+1,s,k,dp));
+        }
+        else {
             int tmp=0;
-            if(cnt<31&&k-(1<<cnt)>=0)
-            {
-                tmp=helper(it+1,cnt+1,s,k-(1<<cnt),dp)+1;
+            if(cnt<31&&k-(1<<cnt)>=0) {
+                tmp=helper(lvl+1,cnt+1,s,k-(1<<cnt),dp)+1;
             }
-            return dp[it][cnt]=max(tmp,helper(it+1,cnt,s,k,dp));
+            return dp[lvl][cnt]=max(tmp,helper(lvl+1,cnt,s,k,dp));
         }
     }
-    
+
     int longestSubsequence(string s, int k) {
-        reverse(s.begin(),s.end());
         vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,-1));
-        int ans=helper(0,0,s,k,dp);
-        return ans;
+        reverse(s.begin(),s.end());
+        return helper(0,0,s,k,dp);
     }
 };
