e91a34bdc69518fdec13ca2764904afb7fbbeceb
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2025-08-13 22:49:10 +0530

    Time: 19 ms (5.68%), Space: 6 MB (100%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==0)return false;
+        while(n%3==0){
+            n/=3;
+        }
+        if(n==1)return true;
+        return false;
+    }
+};
