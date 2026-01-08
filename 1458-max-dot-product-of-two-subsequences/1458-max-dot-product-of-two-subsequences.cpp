c24dd90e06263d77ef7aa5b71b1c281db5b01713
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2026-01-08 23:35:51 +0530

    Time: 75 ms (6.4%), Space: 16.7 MB (70.4%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,21 +1,21 @@
 class Solution {
+    int dp[501][501][2];
 public:
-    
-    int helper(int i1,int i2,vector<int>&nums1,vector<int>&nums2,vector<vector<int>>&dp)
+
+    int helper(int x,int y,int taken,vector<int>&nums1,vector<int>&nums2)
     {
-        if(i1==nums1.size()||i2==nums2.size())
-            return -1e9;
-        
-        if(dp[i1][i2]!=-1e9)
-            return dp[i1][i2];
-        
-        return dp[i1][i2]=max({helper(i1+1,i2,nums1,nums2,dp),helper(i1,i2+1,nums1,nums2,dp),
-                    max(helper(i1+1,i2+1,nums1,nums2,dp),0)+nums1[i1]*nums2[i2]});
-    }
-    
-    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size(),m=nums2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1e9));
-        return helper(0,0,nums1,nums2,dp);
+        if(x>=n||y>=m)
+            return taken>0?0:-1e9;
+        
+        if(dp[x][y][taken]!=-1)
+            return dp[x][y][taken];
+        
+        return dp[x][y][taken]=max({helper(x+1,y,taken,nums1,nums2),helper(x,y+1,taken,nums1,nums2),helper(x+1,y+1,1,nums1,nums2)+nums1[x]*nums2[y]});
+    }
+
+    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+        memset(dp,-1,sizeof(dp));
+        return helper(0,0,0,nums1,nums2);
     }
 };
