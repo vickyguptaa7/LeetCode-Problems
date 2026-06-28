aad4002340c88a225a1fb2636e896c8b1bf62bdd
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2026-06-28 11:25:10 +0530

    Time: 3 ms (94.63%), Space: 55.1 MB (94.35%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -5,11 +5,11 @@ public:
         arr[0]=1;
         for(int i=1;i<arr.size();i++)
         {
-            if(abs(arr[i]-arr[i-1])>1)
+            if(abs(arr[i-1]-arr[i])>1)
             {
                 arr[i]=arr[i-1]+1;
             }
         }
-        return *max_element(arr.begin(),arr.end());
+        return arr.back();
     }
 };
