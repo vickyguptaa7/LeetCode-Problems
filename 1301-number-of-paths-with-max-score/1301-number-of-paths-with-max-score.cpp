592f90d5224c042c6a97e13dfeea6c8e1761619c
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2026-07-05 23:46:46 +0530

    Time: 2469 ms (5.23%), Space: 799.7 MB (5.58%) - LeetHub

---
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+    int helper(int x,int y,vector<string>&board,vector<vector<int>>&dp)
+    {
+        int n=board.size();
+        if(x<0||y<0||x>=n||y>=n||board[x][y]=='X')
+            return -1e9;
+        if(x==0&&y==0)
+        {
+            return 0;
+        }
+        if(dp[x][y]!=-1)
+            return dp[x][y];
+        
+        int val = board[x][y]=='S'?0:board[x][y]-'0';
+        return dp[x][y]=val+max({helper(x-1,y,board,dp),helper(x,y-1,board,dp),helper(x-1,y-1,board,dp)});
+    }
+
+    int helper1(int x,int y,int maxSum,vector<string>&board,vector<vector<vector<int>>>&dp)
+    {
+        int n=board.size();
+        if(x<0||y<0||x>=n||y>=n||board[x][y]=='X'||maxSum<0)
+            return 0;
+        if(x==0&&y==0)
+        {
+            return maxSum==0;
+        }
+        if(dp[x][y][maxSum]!=-1)
+            return dp[x][y][maxSum];
+        
+        int mod=1e9+7;
+        int val = board[x][y]=='S'?0:board[x][y]-'0';
+        long long ans = helper1(x-1,y,maxSum-val,board,dp)+helper1(x,y-1,maxSum-val,board,dp)+helper1(x-1,y-1,maxSum-val,board,dp);
+        return dp[x][y][maxSum]=ans%mod;
+    }
+
+    vector<int> pathsWithMaxScore(vector<string>& board) {
+        int n=board.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        int maxSum=helper(n-1,n-1,board,dp);
+        if(maxSum<0)
+            return {0,0};
+        vector<vector<vector<int>>>dp1(n,vector<vector<int>>(n,vector<int>(9000,-1)));
+        return {maxSum,helper1(n-1,n-1,maxSum,board,dp1)};
+    }
+};
