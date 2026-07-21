39bf6938a763353db86cc03875845e34ed621e22
Vicky Gupta <63355200+vickyguptaa7@users.noreply.github.com>
2026-07-21 22:00:40 +0530

    Time: 85 ms (41.89%), Space: 104.8 MB (29.73%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -15,19 +15,11 @@ public:
             cnt++;
         }
         count.push_back(cnt);
-        if(count.size()==1)
-        {
-            return s[0]=='0'?0:count[0];
-        }
         int ans=ones;
         for(int i=s[0]=='1'?2:1;i<count.size()-1;i+=2)
         {
             ans=max(ans,count[i-1]+ones+count[i+1]);
         }
-        // for(int i=s[0]=='0'?2:1;i<count.size()-1;i+=2)
-        // {
-        //     ans=max(ans,ones+count[i]-1);
-        // }
         return ans;
     }
 };
